bc26032588e25553059d57e6b59df6fd87f9a58a
KevinPramudyanto <kevinpramudyanto92@gmail.com>
2024-10-29 11:02:15 +0800

    Add content for error page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import pizza from "../assets/pizza.png";
 
 const NotFound = () => {
+  const [food, setFood] = useState({});
+
+  const getFood = async () => {
+    try {
+      const res = await fetch("https://foodish-api.com/api/images/pizza");
+      if (!res.ok) {
+        throw new Error("Server Error");
+      }
+      const data = await res.json();
+      setFood(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getFood();
+  }, []);
+
   return (
-    <div className="notfound">
-      <div className="one">The page you are looking for does not exist.</div>
-      <div className="two">Click the navbar above to go back to homepage.</div>
-    </div>
+    <>
+      <div className="notfound">
+        <img
+          src={
+            (food !== null &&
+              typeof food === "object" &&
+              typeof food?.image === "string" &&
+              food?.image) ||
+            pizza
+          }
+        />
+        <div className="one">The page you are looking for does not exist.</div>
+        <div className="two">
+          Click the navbar above to go back to homepage.
+        </div>
+      </div>
+    </>
   );
 };
 
