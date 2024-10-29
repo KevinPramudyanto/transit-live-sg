a82a15f4724140ec1bd5c20478bf4c8f3baaf3d6
KevinPramudyanto <kevinpramudyanto92@gmail.com>
2024-10-29 21:32:50 +0800

    Add NavLink and css for navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <Link to="/all">ALL</Link>
-      <Link to="/bookmarks">BOOKMARKS</Link>
+    <div className="navbarContainer">
+      <Link className="logo" to="/all">
+        TransitLive SG
+      </Link>
+      <div className="navbar">
+        <NavLink
+          className={(navData) => (navData.isActive ? "active" : "notActive")}
+          to="/all"
+        >
+          Main
+        </NavLink>
+        <NavLink
+          className={(navData) => (navData.isActive ? "active" : "notActive")}
+          to="/bookmarks"
+        >
+          Bookmarks
+        </NavLink>
+      </div>
     </div>
   );
 };
