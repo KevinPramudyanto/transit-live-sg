65793997d8da14fb1f357132ca61fc51a69a509d
KevinPramudyanto <kevinpramudyanto92@gmail.com>
2024-10-30 09:49:20 +0800

    Add logo to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import bus from "../assets/bus.png";
 
 const Navbar = () => {
   return (
     <div className="navbarContainer">
       <Link className="logo" to="/all">
-        TransitLive SG
+        <img src={bus} alt="logo" />
+        <div>TransitLive SG</div>
       </Link>
       <div className="navbar">
         <NavLink
